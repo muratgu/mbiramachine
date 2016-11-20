28d2d0ab9b1c9fb57ecb3e2f5a2840c11485e243
muratgu <mgungora@gmail.com>
2016-11-19 20:06:26 -0800

    nyamaropa image

--- mbira-tone.js
+++ mbira-tone.js
@@ -3,23 +3,34 @@
  * requires jquery.js
  * requires tone.js
  */
-var MbiraTone = function(){
-    var TAG = 'MbiraTone'
-    var DEBUG = true
+var MbiraTone = function(callback){
+    const TAG = 'MbiraTone'
+    const DEBUG = true
     
-    var soundFilePath = './sounds'
-    var soundFileExt = '.mp3'
-    var hoshoKitName = 'hosho'
-    var hoshoKey = 'X'
+    const instrumentFilePath = './instruments.json'
+    const soundFilePath = './sounds'
+    const soundFileExt = '.mp3'
+    const hoshoKitName = 'hosho'
+    const hoshoKey = 'X'
 
-    var log = function (s) { if(DEBUG && console && console.log) console.log(s) }
-    var error = function (s, e) { if (console && console.log) console.log(s); console.log(e) }   
-    
+    const log = function (s) { if(DEBUG && console && console.log) console.log(s) }
+    const error = function (s, e) { if (console && console.log) console.log(s); console.log(e) }   
+
     log(TAG+': module initializing')
 
-    var module = {}
+    if (!Array.prototype.flatMap) {
+        Array.prototype.flatMap = function(lambda) { 
+        return Array.prototype.concat.apply([], this.map(lambda)); 
+        }
+    }
 
-    var tabTexts = {
+    var module = {}    
+    var _schedule = null
+    var _instruments = null    
+    var _kits = {}
+    var _tabs = {}
+    var _onBufferLoadCallback = null
+    var _tabTexts = {
         'nyamaropa_kushaura' : `
             R2 L1 / B1 / R2 / L1 / R2 B1 / / R4 L5 / B2 / R4 / L2 / R3 B4 / /
             R2 L1 / B1 / R2 / L1 / R2 B1 / / R4 L5 / B2 / R2 / L4 / R4 B5 / /
@@ -27,58 +38,63 @@ var MbiraTone = function(){
             R3 L4 / B7 / R3 / L4 / R3 B7 / / R5 L3 / B3 / R5 / L4 / R4 B5 / 
             `
     }
+
+    var getInstrumentImageFileName = function(name) {
+        if (_instruments && _instruments[name] && _instruments[name].image)
+            return _instruments[name].image
+    }
+
+    var getInstrumentPathsForKeys = function(name) {
+        log(TAG+': getInstrumentPathsForKeys: name='+name)
+        if (_instruments && _instruments[name]) {
+            var keys = _instruments[name].keys
+            Object.keys(keys).forEach(x=> {
+                keys[x] = new Path2D(keys[x])
+            })
+            return keys
+        }
+    }
     
-    var kits = { }
-
-    var tabs = { }
-
-    // define flatMap is missing
-    if (!Array.prototype.flatMap) {
-        Array.prototype.flatMap = function(lambda) { 
-        return Array.prototype.concat.apply([], this.map(lambda)); 
-        }
+    var loadTabs = function () {
+        getTabNames().forEach(tabName => {
+            _tabs[tabName] = _tabTexts[tabName].split('/').map(x => x.trim().split(' '))
+        })        
     }
 
-    var isUnique = function (value, index, self) { 
-        return self.indexOf(value) === index;
-    }
-
-    var loadTab = function (tabName) {
-        log(TAG+': loadTab: tabName='+tabName)
-        if (!tabs[tabName]) {
-            var tabText = tabTexts[tabName]
-            if (!tabText) {
-                throw new Error('unknown tab name='+tabName)
-            }
-            tabs[tabName] = tabText.split('/').map(x => x.trim().split(' '))
-        }
-        return tabs[tabName]
-    }
-
-    var loadKit = function (kitName) {
-        log(TAG+': loadKit: kitName='+kitName)
-        if (!kits[kitName]) {
-            var keys = [
-                'X','R1','R2','R3','R4','R5','R6','R7','R8','R9'
-                   ,'L1','L2','L3','L4','L5','L6','L7',
-               'B0','B1','B2','B3','B4','B5','B6','B7',                
-            ]
+    var loadKits = function (callback) {
+        _onBufferLoadCallback = callback
+        getKitNames().forEach(kitName => {
+            var keys = Object.keys(_instruments[kitName].keys)
             var urls = {}
             keys.forEach(x => { 
                 urls[x] = sound_file = soundFilePath +
                    '/' + (x == hoshoKey ? hoshoKitName : kitName) +
                    '_' + x + soundFileExt 
             })
-            kits[kitName] = new Tone.MultiPlayer({ urls : urls }).toMaster();
-        }
-        return kits[kitName]
+            _kits[kitName] = new Tone.MultiPlayer({ urls : urls }).toMaster()
+        })
     }
     
+    var getKitNames = function() {
+        if (_instruments) {
+            return Object.keys(_instruments)
+        }
+    }
+
+    var getTabNames = function() {
+        if (_tabTexts) {
+            return Object.keys(_tabTexts)
+        }
+    }
 
     var schedulePlayer = function(kit, tab, tempo, start, onKeysPlayed) {
         log(TAG+': schedulePlayer, tempo='+tempo+', start='+start)
         var tab_index = 0
-        Tone.Transport.scheduleRepeat(function(time){
+        if (_schedule != null) {
+            log(TAG+': schedulePlayer: clear='+_schedule)
+            Tone.Transport.clear(_schedule)
+        }
+        _schedule = Tone.Transport.scheduleRepeat(function(time){
             if (tab_index == tab.length) tab_index = 0
             var keys = tab[tab_index]
             if (onKeysPlayed) onKeysPlayed(keys)
@@ -93,19 +109,19 @@ var MbiraTone = function(){
                     }
                 })            
         }, tempo, start)
+        log(TAG+': schedulePlayer: _schedule='+_schedule)
     }
 
-    var load = function(options, callback) { 
+    var load = function(options) { 
         log(TAG+': load: options=' + options)
         log(options)
         options = options || {}
-        var tabName = options.tabName || 'nyamaropa_kushaura'
-        var kitName = options.kitName || 'dambatsoko_e'
+        var tabName = options.tabName 
+        var kitName = options.kitName 
         var tempo = options.tempo || "8n" // 8th of a note
         var start = options.start || "1m" // one measure        
-        module.onBufferLoadCallback = callback
-        var tab = loadTab(tabName)
-        var kit = loadKit(kitName)
+        var tab = _tabs[tabName]
+        var kit = _kits[kitName]
         schedulePlayer(kit, tab, tempo, start, options.onKeysPlayed)
     }
 
@@ -114,15 +130,26 @@ var MbiraTone = function(){
     
     Tone.Buffer.on('load', function(){
         log(TAG+': Tone.Buffer.onload')
-        if (module.onBufferLoadCallback) module.onBufferLoadCallback() 
+        if (_onBufferLoadCallback) _onBufferLoadCallback() // finally ready
     })
 
-    module.onBufferLoadCallback = null
+    module.getKitNames = getKitNames
+    module.getTabNames = getTabNames
+    module.getInstrumentImageFileName = getInstrumentImageFileName
+    module.getInstrumentPathsForKeys = getInstrumentPathsForKeys
     module.load = load
     module.start = start
     module.stop = stop
-    
-    log(TAG+': module initialized')
+ 
+    $.getJSON(instrumentFilePath, function(result) {
+        _instruments = result
+        loadTabs()
+        loadKits(function(){
+            log(TAG+': instruments loaded')
+            log(Object.keys(_instruments))
+            if (callback) callback() // ready
+        })
+    })
 
     return module
 }
